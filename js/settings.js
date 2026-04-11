0febc68c693165e3793cde17008fafe04fe1504f
SmartNeighborApp <shirit.coach@gmail.com>
2026-04-11 22:49:04 +0300

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -27,12 +27,6 @@ function submitPin(){
   showToast('✅ כניסת מנהל הצליחה');
   var smInp = document.getElementById('admin-start-month');
   if(smInp && DB.building.startMonth) smInp.value = DB.building.startMonth;
-  var feeInp = document.getElementById('admin-fee');
-  if(feeInp && DB.building.monthly_fee) feeInp.value = DB.building.monthly_fee;
-  var unitsInp = document.getElementById('admin-units');
-  if(unitsInp && DB.building.total_units) unitsInp.value = DB.building.total_units;
-  var nameInp = document.getElementById('admin-bldg-name');
-  if(nameInp && DB.building.name) nameInp.value = DB.building.name;
   renderSettingsPage();
   renderCommunityPage();
   updateNavDot();
@@ -686,7 +680,7 @@ function initCollectionCenter(){
   if(!sel) return;
   var now = new Date();
   var html = '';
-  for(var i=-35;i<=1;i++){
+  for(var i=-35;i<=12;i++){
     var d = new Date(now.getFullYear(), now.getMonth()+i, 1);
     var mk = monthKey(d);
     var lbl = fmtMonth(d);
